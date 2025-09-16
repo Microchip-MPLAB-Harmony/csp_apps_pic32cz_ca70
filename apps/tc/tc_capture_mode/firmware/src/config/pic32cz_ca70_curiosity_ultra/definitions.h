fb0667e37703a198404d48723707b72a1dd81289
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:56:56 +0000

    [APP] Regenerated demo for tc__tc_capture_mode__firmware
    MH3C-272

--- apps/tc/tc_capture_mode/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
+++ apps/tc/tc_capture_mode/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -54,8 +54,8 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
-#include "peripheral/tc/plib_tc1.h"
 #include "peripheral/efc/plib_efc.h"
+#include "peripheral/tc/plib_tc1.h"
 #include "peripheral/tc/plib_tc0.h"
 
 // DOM-IGNORE-BEGIN
